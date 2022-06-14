738e9adbb87cb1f4314d8a2e01b782b707063e17
storms22 <traceye5@aol.com>
2022-06-14 17:10:21 +0100

    updating days

--- week6/Day 14/script.js
+++ week6/Day 14/script.js
@@ -5,8 +5,14 @@ function search(value) {
   for (let i = 0; i < li.length; i++) {
     if (li[i].classList.value === "active") {
       li[i].classList.remove("active");
-      console.log(li);
+      console.log(li[i])
     }
+  }
+
+  if(value === "all") {
+    document.querySelectorAll("li").forEach(function(li) {
+      li.classList.add("active");
+    });
   }
   document
     .querySelectorAll(`ul > [data-search~="${value}"]`)
@@ -14,3 +20,32 @@ function search(value) {
       item.classList.add("active");
     });
 }
+
+
+/*function search(value) {
+  alert(value);
+
+  let parent = document.querySelector("ul")
+  console.log(parent.classList)
+  parent.classList.add("filtered")
+
+  search.reset(parent);
+
+  if(value !== "") {
+  console.log(value)
+    document.querySelectorAll(`ul > [data-search~="${value}"]`).forEach(function(item) {
+        console.log(item);
+        item.classList.add("active");
+    });
+  }else{
+      parent.classList.remove("filtered");
+  }
+
+}
+
+search.reset = function(container) {
+  container.querySelectorAll(".active").forEach(function(item){
+      item.classList.remove("active");
+  });
+  
+}*/
